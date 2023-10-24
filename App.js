b5a6d7de00ae10a0b0aa924703168b216286f513
Maksim <93036602+KREIK2017@users.noreply.github.com>
2023-10-24 20:57:30 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,107 +1,82 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, SafeAreaView, StyleSheet } from 'react-native';
+import { Button, StyleSheet, Alert } from 'react-native';
+import { Input, Heading, NativeBaseProvider, Text, Center, Stack } from 'native-base';
 
 export default function App() {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(100);
-  const [backgroundColor, setBackgroundColor] = useState(`rgb(${red}, ${green}, ${blue})`);
-
-  const updateColor = () => {
-    setBackgroundColor(`rgb(${red}, ${green}, ${blue})`);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  // Словник логінів і паролів
+  const users = {
+    user123: 'password123',
+    testuser: 'testpassword',
+    // Додайте більше логінів і паролів за потреби
   };
 
-  const increaseSize = () => {
-    setWidth((prevWidth) => prevWidth + 10);
-    setHeight((prevHeight) => prevHeight + 10);
-  };
+  const styles = StyleSheet.create({
+    containerTop: {
+      marginTop: 40,
+      alignItems: 'center',
+    },
+    center: {
+      height: 40,
+      borderRadius: 10,
+      backgroundColor: 'lightgray',
+    },
+    input: {
+      borderWidth: 3,
+      borderColor: 'gray',
+      borderRadius: 8,
+      padding: 10,
+      marginTop: 10,
+      marginBottom: 10,
+    },
+  });
 
-  const decreaseSize = () => {
-    setWidth((prevWidth) => prevWidth - 10);
-    setHeight((prevHeight) => prevHeight - 10);
+  const userLogin = () => {
+    if (users[email] === password) {
+      // Успішна авторизація
+      Alert.alert('Успішна авторизація', 'Авторизація пройшла успішно', [
+        {
+          text: 'OK',
+        },
+      ]);
+    } else {
+      // Помилка авторизації
+      Alert.alert('Помилка авторизації', 'Неправильний логін або пароль', [
+        {
+          text: 'OK',
+        },
+      ]);
+    }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          width: width,
-          height: height,
-          backgroundColor: backgroundColor,
-        }}
-      ></View>
-      <TextInput
-        placeholder="Введіть R (0-255)"
-        keyboardType="numeric"
-        onChangeText={(text) => setRed(text)}
+    <NativeBaseProvider>
+      <Stack style={styles.containerTop}>
+        <Heading>Registration</Heading>
+      </Stack>
+
+      <Center style={styles.center}>Email:</Center>
+      <Input
+        placeholder="Enter Email..."
+        style={styles.input}
+        onChangeText={(text) => setEmail(text)}
+        value={email}
       />
-      <TextInput
-        placeholder="Введіть G (0-255)"
-        keyboardType="numeric"
-        onChangeText={(text) => setGreen(text)}
+
+      <Center style={styles.center}>Password:</Center>
+      <Input
+        placeholder="Enter Password..."
+        style={styles.input}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true} // Пароль приховується
       />
-      <TextInput
-        placeholder="Введіть B (0-255)"
-        keyboardType="numeric"
-        onChangeText={(text) => setBlue(text)}
-      />
-      <Button title="Змінити колір" onPress={updateColor} />
-      <View style={styles.buttonContainer}>
-        <View style={styles.buttonContainer1}>
-          <Button title="+" onPress={increaseSize} />
-        </View>
-        <View style={styles.buttonContainer1}>
-          <Button title="-" onPress={decreaseSize} />
-        </View>
-      </View>
-      <View style={styles.tableContainer}>
-        <View style={styles.tableRow}>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-        </View>
-        <View style={styles.tableRow}>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-        </View>
-        <View style={styles.tableRow}>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-          <View style={styles.tableCell}></View>
-        </View>
-      </View>
-    </SafeAreaView>
+
+      <Button title="Увійти" onPress={userLogin}>
+        <Text>Увійти</Text>
+      </Button>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginTop: 10,
-  },
-  buttonContainer1: {
-    height: 50,
-    width: 50,
-    marginRight: 10,
-  },
-  tableContainer: {
-    flexDirection: 'column',
-  },
-  tableRow: {
-    flexDirection: 'row',
-  },
-  tableCell: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'lightgray',
-    margin: 5,
-  },
-});
